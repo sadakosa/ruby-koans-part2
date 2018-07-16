cca3e947e47b85c7d20ea60e76a84a41ddab3995
Akira Wong <akira@awongh.com>
2018-07-16 17:16:31 +0800

    Update path_to_enlightenment.rb

--- path_to_enlightenment.rb
+++ path_to_enlightenment.rb
@@ -3,11 +3,13 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
 require 'about_asserts'
-require 'about_triangle_project'
 require 'about_exceptions'
-require 'about_triangle_project_2'
+
 require 'about_classes'
-require 'about_open_classes'
+require 'about_triangle_project'
+
+require 'about_triangle_project_2'
+
 require 'about_dice_project'
 require 'about_inheritance'
 require 'about_class_methods'
