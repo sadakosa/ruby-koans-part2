a3981093a002519da645338ffda34dc2512f6ed8
sadakosa <sabrinachow98@gmail.com>
2018-07-17 11:02:53 +0800

    done some

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  # code ...
+
+  def roll(num)
+    @value = [1,2, 3, 4, 5].shuffle[num]
+  end
+
+  def values
+    @value
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,44 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+def checkIfPy (a, b, c)
+  if c > b && c > a
+    if a + b > c
+      true
+    else
+      false
+    end
+  elsif b > c && b > a
+    if a + c > b
+      true
+    else
+      false
+    end
+  elsif a > b && a > b
+    if c + b > a
+      true
+    else
+      false
+    end
+  end
+end
+
 def triangle(a, b, c)
   # WRITE THIS CODE
+  # begin 
+  check = checkIfPy(a, b, c)
+
+
+  if (a <= 0 || b <= 0 || c <= 0 || check == false)
+    raise TriangleError
+  elsif (a == b && b == c) 
+    :equilateral
+  elsif (a == b || b == c || c == a) 
+    :isosceles
+  else 
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
